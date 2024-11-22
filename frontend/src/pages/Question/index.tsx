22e107e985315e1fce8fd7569a854c470f4788dc
EakkawatP <eakkawat.prommana.dev@gmail.com>
2024-11-22 22:41:42 +0700

    Create pages folder

---
+++ frontend/src/pages/Question/index.tsx
@@ -0,0 +1,12 @@
+import { Card, Text } from '@mantine/core';
+
+function index() {
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Text size="lg" fw={500}>
+        Javascript
+      </Text>
+    </Card>
+  );
+}
+export default index;
