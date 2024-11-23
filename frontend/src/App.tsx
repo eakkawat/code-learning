b16e9dea7cb3351328faaa51c15ab7e33761e5d9
EakkawatP <eakkawat.prommana.dev@gmail.com>
2024-11-23 13:01:09 +0700

    Delete old code comments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,3 @@
-// import { Container } from '@mantine/core';
-// import Quiz from '@/components/Quiz';
 import { Route, Routes } from 'react-router-dom';
 import Quiz from './pages/Quiz';
 import Home from './pages/Home';
@@ -7,9 +5,6 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    // <Container size="sm" py="xl">
-    //   <Quiz id={sampleQuiz.id} title={sampleQuiz.title} questions={sampleQuiz.questions} />
-    // </Container>
     <>
       <header>
         <Navbar />
