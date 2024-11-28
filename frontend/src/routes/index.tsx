b11f8b4a1fd2b1ee4d57db23f2aebae127d3754a
EakkawatP <eakkawat.prommana.dev@gmail.com>
2024-11-28 21:03:25 +0700

    Add not found page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,12 +1,17 @@
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import quizRoutes from './quizRoutes';
 import homeRoutes from './homeRoutes';
+import NotFound from '@/pages/NotFound';
 
 function AppRoutes() {
   return (
     <Routes>
       {homeRoutes}
       {quizRoutes}
+      <Route
+        path="*"
+        element={<NotFound />}
+      />
     </Routes>
   );
 }
