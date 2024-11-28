d51b496b88bc938e7b592c71b6eea91798c1ae30
EakkawatP <eakkawat.prommana.dev@gmail.com>
2024-11-28 17:00:42 +0700

    Experiment: pull quiz data from json

--- frontend/src/routes/quizRoutes.tsx
+++ frontend/src/routes/quizRoutes.tsx
@@ -1,20 +1,6 @@
 import { Route } from 'react-router-dom';
+import questions from '../../../data.json';
 import Quiz from '../pages/Quiz';
-
-const questions = [
-  {
-    id: 'q1',
-    question: 'What is JavaScript primarily used for?',
-    options: ['Web Development', 'Database Management', 'Operating System Development', 'Mobile App Development'],
-    correctAnswer: 'Web Development',
-  },
-  {
-    id: 'q2',
-    question: 'Which keyword is used to declare variables in JavaScript?',
-    options: ['var', 'let', 'const', 'All of the above'],
-    correctAnswer: 'All of the above',
-  },
-];
 
 const quizRoutes = [
   <Route
